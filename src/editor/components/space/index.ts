33c2c840c39b2bab44960bd0f2b4b2daceeaadf3
huangting <huangting@vv.cn>
2025-12-01 22:01:55 +0800

    feat: 改写Space组件，以组件为单位，重新设计模块关系

---
+++ src/editor/components/space/index.ts
@@ -0,0 +1,62 @@
+import SpaceDev from "./dev";
+import SpaceProd from "./prod";
+// import type {ComponentConfig} from "../../interface";
+import type {Context} from "../../interface";
+
+// 对外暴露一个注册组件的方法，这个方法可以同步/异步的返回组件
+// 的配置，我们可以在任何地方和时机调用这个方法，进行组件注册
+export default (ctx: Context) => {
+  ctx.registerComponent("Space", {
+    name: "Space",
+    desc: "间距",
+    defaultProps: {
+      size: {
+        type: "static",
+        value: "middle",
+      },
+      direction: {
+        type: "static",
+        value: "horizontal",
+      },
+    },
+    dev: SpaceDev,
+    prod: SpaceProd,
+    setter: [
+      {
+        name: "size",
+        label: "间距大小", // 字段名称
+        type: "select",
+        options: [
+          {
+            label: "小",
+            value: "small",
+          },
+          {
+            label: "中",
+            value: "middle",
+          },
+          {
+            label: "大",
+            value: "large",
+          },
+        ],
+      },
+      {
+        name: "direction",
+        label: "方向",
+        type: "select",
+        options: [
+          {
+            label: "垂直方向",
+            value: "vertical",
+          },
+          {
+            label: "水平方向",
+            value: "horizontal",
+          },
+        ],
+      },
+    ],
+    order: 1,
+  });
+};
